2b5727fd8df668a9003e30f4d3267363a61cb91a
Igor Igor <iyashtykov@codio.com>
2024-05-21 15:47:37 +0000

    fix

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-codioIDE.coachBot.register("name|id", showLevel1)
-function showLevel1(data) {
-  if (data === undefined)
-     var data = await codioIDE.coachBot.input()
-   }
-   if (data == "lalala") {
-    codioIDE.coachBot.showButton("Yay", onYay)
-    codioIDE.coachBot.showButton("No", onNo)
-   }
-    codioIDE.coachBot.showButton("Help", () =>onHelp(data))   
-}
+window.codioIDE.coachBot.register('customButtonId', 'try new api', () => {
+    window.codioIDE.coachBot.showButton('empty callback', () => {
+        console.log('empty callback button was clicked')
+    })
+    window.codioIDE.coachBot.showButton('try with input', async () => {
+        console.log('with input button was clicked')
+        const input = await window.codioIDE.coachBot.input('enter some error please')
+        console.log('input', input)
+        const result = await window.codioIDE.coachBot.ask({userPrompt: 'use effect', systemPrompt: 'provide react hook documentation for user defined hook'}, {stream: false})
+        console.log('hidden ask result', result)
 
-function onHelp(data) {
-    codioIDE.coachBot.showButton("react Help", onHelpReact)
-    codioIDE.coachBot.showButton("js help", onHelpJs)
-    codioIDE.coachBot.showButton("Back", () => {showLevel1 (data)})
-}
+        window.codioIDE.coachBot.ask({userPrompt: input, systemPrompt: 'explain user defined error'})
+    })
