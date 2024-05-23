7312ca9406cdcbfa2e31c9486eac5ed8fe60903b
Igor Igor <iyashtykov@codio.com>
2024-05-23 13:45:26 +0000

    levels

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-window.codioIDE.coachBot.register('customButtonId', 'try new api', () => {
-    window.codioIDE.coachBot.showButton('empty callback', () => {
-        console.log('empty callback button was clicked')
+window.codioIDE.coachBot.register('level1button', 'level1 button', () => {
+    window.codioIDE.coachBot.showButton('level2 input', async () => {
+        const input = await window.codioIDE.coachBot.input('enter hook name')
+        window.codioIDE.coachBot.write('Please wait. I will provide info shortly...')
+        window.codioIDE.coachBot.ask({userPrompt: input, systemPrompt: 'show react hook documentation for user provided hook'})
     })
-    window.codioIDE.coachBot.showButton('try with input', async () => {
-        console.log('with input button was clicked')
-        const input = await window.codioIDE.coachBot.input('enter some error please')
-        console.log('input', input)
-        const result = await window.codioIDE.coachBot.ask({userPrompt: 'use effect', systemPrompt: 'provide react hook documentation for user defined hook'}, {stream: false})
-        console.log('hidden ask result', result)
-
-        window.codioIDE.coachBot.ask({userPrompt: input, systemPrompt: 'explain user defined error'})
+    window.codioIDE.coachBot.showButton('level2 button', async () => {
+        window.codioIDE.coachBot.ask({userPrompt: 'level2 button', systemPrompt: 'level2 button'})
     })
+})
+window.codioIDE.coachBot.register('level1input', 'level1 input', async () => {
+    const input = await window.codioIDE.coachBot.input('enter input value')
+    window.codioIDE.coachBot.write('Please wait...')
+    window.codioIDE.coachBot.ask({userPrompt: input, systemPrompt: 'show input'})
 })
