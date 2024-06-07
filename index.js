7dc8c247c955ea58fe40d55c4be7b4cb31d9a280
Igor Yashtykov <iyashtykov@users.noreply.github.com>
2024-06-07 10:50:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,3 +24,20 @@ window.codioIDE.coachBot.register('level1input', 'level1 input', async () => {
     window.codioIDE.coachBot.write('Please wait in level1...')
     window.codioIDE.coachBot.ask({userPrompt: input, systemPrompt: 'show input'})
 })
+window.codioIDE.coachBot.register('analyseBtnId', 'Analyse history and what next', async () => {
+  const messages = await window.codioIDE.coachBot.getHistory()
+  console.log(messages)
+  let currentMessageRole = window.codioIDE.coachBot.MESSAGE_ROLES.ASSISTANT // initial value to get next message with role "USER"
+  const messagesToSend = messages.reduce((acc, message) => {
+    if (message.role !== currentMessageRole) {
+      acc.push(message)
+      currentMessageRole = message.role
+    }
+    return acc
+  }, [])
+  try {
+    await window.codioIDE.coachBot.ask({systemPrompt: 'analyse history and provide a hint what should user do next', messages: messagesToSend})
+  } catch (e) {
+    console.log('coach bot error on ask', e.message)
+  }
+})
