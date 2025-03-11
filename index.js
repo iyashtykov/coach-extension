8b9e4901f76500d4c7c353b55b437117ac9a09ad
Igor Yashtykov <iyashtykov@users.noreply.github.com>
2025-03-11 14:30:43 +0000

    Update system promts

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ window.codioIDE.coachBot.register('level1button', 'level1 button', () => {
         window.codioIDE.coachBot.ask({userPrompt: input, systemPrompt: 'show react hook documentation for user provided hook'})
     })
     window.codioIDE.coachBot.showButton('level2 button promt', async () => {
-        window.codioIDE.coachBot.ask({userPrompt: 'level2 button promt', systemPrompt: 'level2 button promt'})
+        window.codioIDE.coachBot.ask({userPrompt: 'python indention', systemPrompt: 'show react hook documentation for user provided hook'})
     })
     window.codioIDE.coachBot.showButton('level2 button', async () => {
         window.codioIDE.coachBot.showButton('level3 button', async() => {
-            window.codioIDE.coachBot.ask({userPrompt: 'level3 button promt', systemPrompt: 'level3 button promt'})
+            window.codioIDE.coachBot.ask({userPrompt: 'java indention', systemPrompt: 'show react hook documentation for user provided hook'})
         })
         window.codioIDE.coachBot.showButton('level3 input', async () => {
             const input = await window.codioIDE.coachBot.input('enter name in level3')
             window.codioIDE.coachBot.write('Please wait in level3...')
-            window.codioIDE.coachBot.ask({userPrompt: input, systemPrompt: 'level3 input'})
+            window.codioIDE.coachBot.ask({userPrompt: input, systemPrompt: 'show react hook documentation for user provided hook'})
         })        
         
     })    
